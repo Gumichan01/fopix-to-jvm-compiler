dbbf3c12cb455cadf14a4634507a89270a491483
Yves Viaud <viaud@informatique.univ-paris-diderot.fr>
2018-01-25 11:30:08 +0100

    fopixToJavix: Added a Box to Num to hint the need for list

--- src/javix/fopixToJavix.ml
+++ src/javix/fopixToJavix.ml
@@ -115,7 +115,7 @@ let rec translate p env : T.t * environment =
      Maybe I am wrong but we should ask the teacher for confirmation
   *)
   and translate_expr env = function
-    | S.Num i -> None, T.Bipush(i)
+    | S.Num i -> (None, T.Bipush(i))::(None, T.Box)::[]
     | S.FunName fn ->
       (*let _ = { nextvar = env.nextvar; variables = env.variables;
                    function_labels = (fn, T.Label(fn))::env.function_labels;
