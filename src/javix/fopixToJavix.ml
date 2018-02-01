fee4b011bc2252726aa1f0ba781cc649f14e04d9
Yves Viaud <viaud@informatique.univ-paris-diderot.fr>
2018-02-01 11:49:43 +0100

    fopixToJavix: BlockSet implementation attempt

--- src/javix/fopixToJavix.ml
+++ src/javix/fopixToJavix.ml
@@ -164,9 +164,12 @@ let rec translate p env : T.t * environment =
       let i = translate_expr env e2 in
       b @ i @ (None, T.AAload)::[]
 
-    | S.BlockSet _ ->
-      failwith "BlockSet - Students! this is your job!"
-        (* Do not forget to add a Bipush 0 in order to return a val ! *)
+    | S.BlockSet (e1,e2,e3) ->
+      let b = translate_expr env e1 in
+      let i = translate_expr env e2 in
+      let v = translate_expr env e3 in
+      (* Adding a T.Bipush(0) as a return value for the S.DefVal *)
+      b @ i @ v @ (None, T.AAstore)::(None, T.Bipush(0))::[]
 
     | S.FunCall _ ->
       failwith "FunCall - Students! this is your job!"
