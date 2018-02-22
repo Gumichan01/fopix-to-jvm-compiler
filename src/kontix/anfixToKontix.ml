17a929736487db9d149f1fe79624f231da4297d3
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2018-02-22 14:05:55 +0100

    anfix to kontix: started translate

--- src/kontix/anfixToKontix.ml
+++ src/kontix/anfixToKontix.ml
@@ -12,8 +12,7 @@ type environment = unit (* TODO *)
 let initial_environment () = () (* TODO *)
 
 let rec translate (p : S.t) (env : environment) = (*failwith "TODO translate" : T.t * environment*)
-  let ldef = retrieve_dval p in
-  let fdef = retrieve_dfun p in
+  let ldef, fdef = retrieve_definitions p in
   (* Just to test *)
   print_string("hello "^ string_of_int(List.length ldef) ^" - "
                ^ string_of_int(List.length fdef)); print_endline(""); (([],T.TContCall(T.Print("exit"))), env)
@@ -32,21 +31,10 @@ let rec translate (p : S.t) (env : environment) = (*failwith "TODO translate" : 
   | _::q -> retrieve_dfun q
   )
 
-  (*match p with
-  | [] -> ([], T.TContCall(T.Print("")))
-  | d::q ->
-    let cd = translate_def env d in
-    ( cd :: , T.TContCall(T.Print("")))*)
-
-  (*let rec translate_aux prog ev acc =
-  match prog with
-  | [] -> acc
-  | d::q -> translate_aux q ev (acc @ translate_def env d)
-  in
-  translate_aux p env []*)
-
-  (*and translate_def env = function
-  | S.DefVal(_,_)   -> failwith "DefVal - TODO"
-  | S.DefFun(_,_,_) -> failwith "DefFun - TODO"*)
-
-(*(failwith "TODO translate" : T.t * environment)*)
+  and retrieve_definitions p =
+    let rec aux_retrieve p (d, f) =
+      match p with
+      | [] -> (d, f)
+      | (S.DefVal(_) as dv)::q -> aux_retrieve q ((d @ [dv]), f)
+      | (S.DefFun(_) as df)::q -> aux_retrieve q (d, (f @ [df]))
+    in aux_retrieve p ([], [])
